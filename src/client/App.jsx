5847db3293956d8065bd186ab9689e84792d19d2
Adam <adam.pol1234@gmail.com>
2017-10-07 01:06:32 +0200

    feat(js): add login component in app route

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
+import { Route, Switch } from 'react-router-dom';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
@@ -11,6 +12,8 @@ import Content from './components/Content';
 import classnames from 'classnames/bind';
 
 import styles from './App.css';
+
+import Login from './components/Login';
 
 const cx = classnames.bind(styles);
 // import DevTools from 'mobx-react-devtools';
@@ -36,18 +39,26 @@ class App extends Component {
   render() {
     // const { location, push, goBack } = this.props.themeStore;
     const { darkTheme } = this.props.themeStore;
+
     return (
       <MuiThemeProvider muiTheme={darkTheme ? getMuiTheme(darkBaseTheme) : null}>
-        <div className={cx('clearfix')}>
-          <TopBar />
-          <Menu handleChange={this.handleChange} />
-          <Content />
-          
-          {/*<span>Current pathname: {location.pathname}</span>
-          <button onClick={() => push('/test')}>Change url</button>
-          <button onClick={() => goBack()}>Go Back</button>*/}
-          {/* <DevTools /> */}
-        </div>
+
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route path="/" children={({match}) => (
+            <div className={cx('clearfix')}>
+              <TopBar />
+              <Menu handleChange={this.handleChange} />
+              <Content />
+              
+              {/*<span>Current pathname: {location.pathname}</span>
+              <button onClick={() => push('/test')}>Change url</button>
+              <button onClick={() => goBack()}>Go Back</button>*/}
+              {/* <DevTools /> */}
+            </div>
+
+          )} />
+        </Switch>
       </MuiThemeProvider>
 
     );
