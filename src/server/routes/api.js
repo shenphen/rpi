faf2a6aaa707f2179ac0fa17b8d8767c28178a1c
Adam <adam.pol1234@gmail.com>
2017-10-17 21:41:34 +0200

    feat(js): add get params api route

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import db from '../lib/redis.js';
+const debug = require('debug')('api');
 
 const router = new express.Router();
 
@@ -18,6 +19,33 @@ router.post('/params', (req, res, next) => {
     else {
         res.json({
             status: 'NOT OK',
+        })
+    }
+})
+
+router.get('/params', (req, res, next) => {
+    const avaibleParams = ['temperature', 'humidity'];
+    if(req.body && req.body.from && req.body.end && avaibleParams.indexOf(req.body.param) !== -1) {
+        const FROM = parseInt(req.body.from, 10) || 0;
+        const TO = parseInt(req.body.end, 10) || 0;
+
+        db.send_commandAsync('TS.RANGE', [req.body.param, FROM, TO])
+        .then(result => {
+            console.log(result);
+            
+            res.json({
+                status: 'OK',
+                data: {'YOU_LUCKY': 'GUY'}
+            })
+        })
+        .catch(err => {
+            debug(err);
+        });
+    }
+    else {
+        res.json({
+            data: {},
+            status: 'BAD_QUERY'
         })
     }
 })
